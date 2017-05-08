e66b6f5b9363cef84d310ad136f19d7fec3cbcba
exagradia <exagradia@outlook.com>
2017-05-08 10:47:18 +0700

    Add Dummy Class

---
+++ src/main/java/jboos/common/HelloWorld.java
@@ -0,0 +1,16 @@
+package jboos.common;
+
+public class HelloWorld {
+	
+	String name;
+	
+	public String HelloName(String name){
+		this.name = name;
+		return this.name;
+	}
+	
+	public void HelloDunia(){
+		System.out.println("HelloWorld");
+	}
+
+}
